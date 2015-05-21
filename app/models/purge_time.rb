1b6c2c8657248ae5fb9bb2b75c0a060937ab68fd
TomStroh <tom.stroh@gmx.de>
2015-05-21 22:43:51 +0200

    changes purge

--- app/models/purge_time.rb
+++ app/models/purge_time.rb
@@ -4,16 +4,17 @@ class Purge_time
 def self.update(val)
   # in file herein schreiben
   # val ist der neue wert
-  File.open("/home/pi/security-camera/ini.txt", "w") { |file| file.write(val)}
+  File.open('/home/pi/security-camera/ini.txt', 'w') { |file| file.write(val)}
 
 
 end
 
 def self.show
   # aus File heraus lesen
-  file = File.open("/home/pi/security-camera/ini.txt")
+  file = File.open('/home/pi/security-camera/ini.txt')
   time = file.read
 
 end
+
 
 end
