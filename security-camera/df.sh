52225f462b615823df06b3aa1b4fdec83a739281
etzam <matthias.traub93@web.de>
2015-06-16 16:39:55 +0200

    Update df.sh

--- security-camera/df.sh
+++ security-camera/df.sh
@@ -5,7 +5,7 @@ do
 # freier Speicherplatz auslesen
 size=$(df -k /dev/root | tail -1 | awk '{print $4}')
 #wenn Speicherplatz kleiner 10 MB, sollen älteste Aufzeichnungen gelöscht werden
-if (($size > 10000)); then
+if (($size < 10000)); then
 # Auflisten aller vorhandenen Aufzeichnungen nach Zeit, danach löschen der ältesten Datei
 	cd /home/pi/motionui/public/video/ 
 	ls -t | sed -e '1,1d' | xargs -d '\n' rm
